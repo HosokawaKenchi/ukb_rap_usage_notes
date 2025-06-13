a7658333ef7ee84cc7d3be67ebd2e9ed336933c3
root <hayashizaki.yu.t5@dc.tohoku.ac.jp>
2025-06-13 09:59:26 +0000

    add: tree info

--- scripts/20250613_tree_brain_zip_files.sh
+++ scripts/20250613_tree_brain_zip_files.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-SEARCH_DIR="/mnt/project/Bulk/Brain MRI/T1"
+SEARCH_DIR="/mnt/project/Bulk/Brain MRI/Surface-based analysis of resting and task fMRI"
 OUTPUT_DIR="./docs/tree"
 PYTHON_SCRIPT="./tree_zip.py"
+
+# ASL
+# Connectomes
+# dMRI
+# Functional time series
+# Native atlases
+# Native atlases (Diffusion)
+# Native atlases (Structural and functional)
+# rfMRI
+# Scout
+# Surface-based analysis of resting and task fMRI
 
 # find コマンドの出力を改行区切りで読み込み、配列に格納
 zip_files=()
